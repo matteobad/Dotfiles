7f59cffb1d63dcc135063dca9c7962a7e0f2e5b0
matteobad <matteo.badini@studio.unibo.it>
2016-12-02 20:37:17 +0100

    remove obsolete font

--- font/install.sh
+++ font/install.sh
@@ -4,7 +4,6 @@ echo "Installing fonts"
 
 brew tap caskroom/fonts
 
-brew cask install font-sauce-code-powerline
 brew cask install font-source-code-pro
 brew cask install font-source-sans-pro
 brew cask install font-source-serif-pro
